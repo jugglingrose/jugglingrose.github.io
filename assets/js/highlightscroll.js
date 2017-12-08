471c7cf384a1e08404dc382471bcf8453949934f
Corean Gonzales <coreangonzales@gmail.com>
2017-12-07 22:26:42 -0600

    functioning nav bar

--- assets/js/highlightscroll.js
+++ assets/js/highlightscroll.js
@@ -3,18 +3,22 @@ $(document).ready(function(){
   //activate on window scroll//
   $(document).scroll(function(){
     console.log("window Scroll Top: " + $(window).scrollTop() );
+    if ( $(document).scrollTop() < $('#about').offset().top ){
+      $('nav a').removeClass('active');
+    }
     //loop through each section//
     $('.section').each(function(){
         //if scroll top of window is greater than the section top position
         //grab the id of the current section and match it to the nav anchor//
         //Highlight the matching nav anchor//
+
         if($(document).scrollTop() >= (($(this).offset().top) - 40)){
           console.log("div offset top:" + $(this).offset().top );
           var id = $(this).attr('id');
           $('nav a').removeClass('active');
           $('nav a[href=\\#' + id + ']').addClass('active');
+          }
 
-        };
       });
     });
   });
