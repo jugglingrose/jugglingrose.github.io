9801e6ebc022109d5d9dfc9efa25beda0ff4b7cf
Corean Gonzales <coreangonzales@gmail.com>
2017-12-11 12:47:52 -0600

    fully functioning responsive nav bar completed

--- assets/js/highlightscroll.js
+++ assets/js/highlightscroll.js
@@ -5,9 +5,19 @@ $(document).ready(function(){
   //activate on window scroll//
   $(document).scroll(function(){
     console.log( "document scroll top is:" + $(document).scrollTop() );
+    // if document is in hero landing, remove active from all anchor tags//
     if ( $(document).scrollTop() < $('#about').offset().top ){
       $('nav ul li a').removeClass('active');
     }
+
+    else if ( $(window).scrollTop() + $(window).height() === $(document).height() ){
+        console.log('bottom');
+        $('nav ul li a').removeClass('active');
+        $('nav ul li a[href=\\#contact]').addClass('active');
+    }
+
+    else{
+
     //loop through each section//
     $('.section').each(function(){
         //if scroll top of window is greater than the section top position
@@ -19,7 +29,14 @@ $(document).ready(function(){
           $('nav ul li a').removeClass('active');
           $('nav ul li a[href=\\#' + id + ']').addClass('active');
           }
-
+        });
+      }
       });
     });
-  });
+
+
+/*  else if ( $(window).scrollTop() + $(window).height() === $(document).height() ){
+    alert('bottom');
+    $('nav ul li a').removeClass('active');
+    $('nav ul li a[href=\\#contact]').addClass('active');
+  }*/
